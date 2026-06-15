4920f7736c34b3bfc9877fe57014e0936e18ca4d
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-06-15 15:26:24 -0500

    Pequeño arreglo en la clase DeleteClienteComand

--- src/main/java/com/rabb/clientsmanagement/application/service/dto/command/DeleteClienteCommand.java
+++ src/main/java/com/rabb/clientsmanagement/application/service/dto/command/DeleteClienteCommand.java
@@ -2,7 +2,7 @@ package com.rabb.clientsmanagement.application.service.dto.command;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record DeleteClienterCommand(
+public record DeleteClienteCommand(
     @NotBlank(message = "id must not be blank") String id
 ) {
 

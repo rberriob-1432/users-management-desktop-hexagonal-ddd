870001a12e8405b8ab4e8bfd0e5966f8a05e12e8
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-06-15 17:01:53 -0500

    Excepcion persistencia

---
+++ src/main/java/com/rabb/clientsmanagement/infrastructure/adapter/persistence/exception/PersistenceException.java
@@ -0,0 +1,48 @@
+package com.rabb.clientsmanagement.infrastructure.adapter.persistence.exception;
+
+public final class PersistenceException extends RuntimeException {
+
+  private static final String MESSAGE_SAVE = "Failed to save cliente with ID: '%s'.";
+  private static final String MESSAGE_UPDATE = "Failed to update cliente with ID: '%s'.";
+  private static final String MESSAGE_FIND = "Failed to find cliente with ID: '%s'.";
+  private static final String MESSAGE_EMAIL = "Failed to find cliente with email: '%s'.";
+  private static final String MESSAGE_ALL = "Failed to retrieve all clientes.";
+  private static final String MESSAGE_DELETE = "Failed to delete cliente with ID: '%s'.";
+  private static final String MESSAGE_CONNECTION = "Could not establish database connection.";
+
+  private PersistenceException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+
+  public static PersistenceException becauseSaveFailed(final String clienteId, final Throwable cause) {
+    return new PersistenceException(String.format(MESSAGE_SAVE, clienteId), cause);
+  }
+
+  public static PersistenceException becauseUpdateFailed(
+      final String clienteId, final Throwable cause) {
+    return new PersistenceException(String.format(MESSAGE_UPDATE, clienteId), cause);
+  }
+
+  public static PersistenceException becauseFindByIdFailed(
+      final String clienteId, final Throwable cause) {
+    return new PersistenceException(String.format(MESSAGE_FIND, clienteId), cause);
+  }
+
+  public static PersistenceException becauseFindByEmailFailed(
+      final String email, final Throwable cause) {
+    return new PersistenceException(String.format(MESSAGE_EMAIL, email), cause);
+  }
+
+  public static PersistenceException becauseFindAllFailed(final Throwable cause) {
+    return new PersistenceException(MESSAGE_ALL, cause);
+  }
+
+  public static PersistenceException becauseDeleteFailed(
+      final String clienteId, final Throwable cause) {
+    return new PersistenceException(String.format(MESSAGE_DELETE, clienteId), cause);
+  }
+
+  public static PersistenceException becauseConnectionFailed(final Throwable cause) {
+    return new PersistenceException(MESSAGE_CONNECTION, cause);
+  }
+}

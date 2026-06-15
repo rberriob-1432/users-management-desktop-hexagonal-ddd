a5b5191f5f302a9e9dcb610d7913d5ac6312a6a6
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-06-15 17:53:48 -0500

    Modificando cliente response

--- src/main/java/com/rabb/clientsmanagement/infrastructure/entrypoint/desktop/dto/ClienteResponse.java
+++ src/main/java/com/rabb/clientsmanagement/infrastructure/entrypoint/desktop/dto/ClienteResponse.java
@@ -1,11 +1,10 @@
 package com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.dto;
-
 public record ClienteResponse(
         String id,
         String name,
         String email,
-        String password,
         String role,
+        String status,
         String barrio,
         String calle,
         String city) {}

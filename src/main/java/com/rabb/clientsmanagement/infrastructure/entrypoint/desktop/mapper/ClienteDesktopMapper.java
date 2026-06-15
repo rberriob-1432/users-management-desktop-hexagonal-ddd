422b4bf7a0f55e138240fdaebe95ec65e2dbd7df
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-06-15 17:47:38 -0500

    Modificando mapper

--- src/main/java/com/rabb/clientsmanagement/infrastructure/entrypoint/desktop/mapper/ClienteDesktopMapper.java
+++ src/main/java/com/rabb/clientsmanagement/infrastructure/entrypoint/desktop/mapper/ClienteDesktopMapper.java
@@ -1,10 +1,14 @@
 package com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.mapper;
 
 import com.rabb.clientsmanagement.application.service.dto.command.CreateClienteCommand;
+import com.rabb.clientsmanagement.application.service.dto.command.DeleteClienteCommand;
+import com.rabb.clientsmanagement.application.service.dto.command.LoginCommand;
 import com.rabb.clientsmanagement.application.service.dto.command.UpdateClienteCommand;
+import com.rabb.clientsmanagement.application.service.dto.query.GetClienteByIdQuery;
 import com.rabb.clientsmanagement.domain.model.ClienteModel;
 import com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.dto.ClienteResponse;
 import com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.dto.CreateClienteRequest;
+import com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.dto.LoginRequest;
 import com.rabb.clientsmanagement.infrastructure.entrypoint.desktop.dto.UpdateClienteRequest;
 
 import java.util.List;
@@ -41,7 +45,7 @@ public final class ClienteDesktopMapper {
 
 
 
-  public static ClienteResponse toResponse(final ClienteModel cliente) {
+  public static ClienteResponse toResponse(final ClienteModel cliente) {//Relacionado
     return new ClienteResponse(
             cliente.getId().value(),
             cliente.getName().value(),
@@ -55,5 +59,16 @@ public final class ClienteDesktopMapper {
 
   public static List<ClienteResponse> toResponseList(final List<ClienteModel> clientes) {
     return clientes.stream().map(ClienteDesktopMapper::toResponse).toList();
+  }
+  public static GetClienteByIdQuery toGetByIdQuery(final String id) {
+    return new GetClienteByIdQuery(id);
+  }
+
+  public static DeleteClienteCommand toDeleteCommand(final String id) {
+    return new DeleteClienteCommand(id);
+  }
+
+  public static LoginCommand toLoginCommand(final LoginRequest request) {
+    return new LoginCommand(request.email(), request.password());
   }
 }

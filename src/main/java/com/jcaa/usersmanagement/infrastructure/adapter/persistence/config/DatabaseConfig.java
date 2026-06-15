b94a2bcc3fb6f3e29a0e4f834c32638a51e65e1f
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-06-15 14:34:53 -0500

    configurando de manera local

--- src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
@@ -1,11 +1,11 @@
 package com.jcaa.usersmanagement.infrastructure.adapter.persistence.config;
 
 public record DatabaseConfig(
-    String host, int port, String databaseName, String username, String password) {
-  private static final String URL_TEMPLATE =
-      "jdbc:mysql://%s:%d/%s?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String host, int port, String databaseName, String username, String password) {
+    private static final String URL_TEMPLATE =
+            "jdbc:postgresql://%s:%d/%s";
 
-  public String buildJdbcUrl() {
-    return String.format(URL_TEMPLATE, host, port, databaseName);
-  }
+    public String buildJdbcUrl() {
+        return String.format(URL_TEMPLATE, host, port, databaseName);
+    }
 }
